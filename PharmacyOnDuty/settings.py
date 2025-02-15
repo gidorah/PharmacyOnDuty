44e4d39a56f6eb4a312ec9dd1d28b9393e34da94
Onur Akyüz <onur_akyuz@icloud.com>
2025-02-15 11:43:09 +0300

    feat: Add django_celery_results to settings and update result backend configuration

--- PharmacyOnDuty/settings.py
+++ PharmacyOnDuty/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     "django_browser_reload",
     "django.contrib.sitemaps",
     "django_celery_beat",
+    "django_celery_results",
 ]
 
 TAILWIND_APP_NAME = "theme"
@@ -204,5 +205,5 @@ if DEBUG and REMOTE_DEBUGGING_PORT:
 
 # Celery settings
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
-CELERY_RESULT_BACKEND = "django-db"
+CELERY_RESULT_BACKEND = "django_celery_results.backends.database:DatabaseBackend"
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
