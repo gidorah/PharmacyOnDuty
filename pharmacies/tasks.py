f3272d297e07086ab18850e5d763a951b2ae658e
Onur Akyüz <onur_akyuz@icloud.com>
2025-02-15 20:24:36 +0300

    feat: Add logging for existing scraper configurations in run_scraper function

--- pharmacies/tasks.py
+++ pharmacies/tasks.py
@@ -18,6 +18,10 @@ def run_scraper(city_name):
         add_scraped_data_to_db(city_data, city_name=city_name)
         print(f"Scraper data for city {city_name} saved to DB")
 
+        all_scrapers = ScraperConfig.objects.all()
+        for scraper in all_scrapers:
+            print(f"Scraper config for city {scraper.description} exists")
+
         scraper = ScraperConfig.objects.get(description=city_name)
         print(f"Updating scraper config for city {city_name}")
         scraper.last_run = timezone.now()
