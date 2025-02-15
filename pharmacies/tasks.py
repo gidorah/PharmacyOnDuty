d0db1821706851e10bbe1f569cf0c635654657b5
Onur Akyüz <onur_akyuz@icloud.com>
2025-02-15 13:23:05 +0300

    feat: Add logging to run_scraper for better debugging and tracking

--- pharmacies/tasks.py
+++ pharmacies/tasks.py
@@ -11,13 +11,17 @@ from pharmacies.utils import (
 
 @shared_task
 def run_scraper(city_name):
+    print(f"Running scraper for city {city_name}")
     try:
         city_data = get_city_data(city_name=city_name)
+        print(f"Scraper for city {city_name} finished")
         add_scraped_data_to_db(city_data, city_name=city_name)
+        print(f"Scraper data for city {city_name} saved to DB")
 
         scraper = ScraperConfig.objects.get(city__name=city_name)
-
+        print(f"Updating scraper config for city {city_name}")
         scraper.last_run = timezone.now()
         scraper.save()
+        print(f"Scraper config for city {city_name} updated")
     except Exception as e:
         print(f"Scraper failed: {str(e)}")
