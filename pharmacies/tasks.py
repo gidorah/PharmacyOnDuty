c7a68f9f6f57e1f73051fc1730b6bad9681137b5
Onur Akyüz <onur_akyuz@icloud.com>
2025-02-20 17:11:10 +0000

    remove exception handling in scraper task

--- pharmacies/tasks.py
+++ pharmacies/tasks.py
@@ -12,20 +12,17 @@ from pharmacies.utils import (
 @shared_task
 def run_scraper(city_name):
     print(f"Running scraper for city {city_name}")
-    try:
-        city_data = get_city_data(city_name=city_name)
-        print(f"Scraper for city {city_name} finished")
-        add_scraped_data_to_db(city_data, city_name=city_name)
-        print(f"Scraper data for city {city_name} saved to DB")
+    city_data = get_city_data(city_name=city_name)
+    print(f"Scraper for city {city_name} finished")
+    add_scraped_data_to_db(city_data, city_name=city_name)
+    print(f"Scraper data for city {city_name} saved to DB")
 
-        # all_scrapers = ScraperConfig.objects.all()
-        # for scraper in all_scrapers:
-        #     print(f"Scraper config for city {scraper.description} exists")
+    # all_scrapers = ScraperConfig.objects.all()
+    # for scraper in all_scrapers:
+    #     print(f"Scraper config for city {scraper.description} exists")
 
-        # scraper = ScraperConfig.objects.get(description=city_name)
-        # print(f"Updating scraper config for city {city_name}")
-        # scraper.last_run = timezone.now()
-        # scraper.save()
-        # print(f"Scraper config for city {city_name} updated")
-    except Exception as e:
-        print(f"Scraper failed: {str(e)}")
+    # scraper = ScraperConfig.objects.get(description=city_name)
+    # print(f"Updating scraper config for city {city_name}")
+    # scraper.last_run = timezone.now()
+    # scraper.save()
+    # print(f"Scraper config for city {city_name} updated")
