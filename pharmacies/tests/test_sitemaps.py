2425acc886b33836fa1cd426c21da383ea4ef0ff
Onur Akyüz <onur.accounts@gmail.com>
2025-12-29 20:13:27 +0300

    ♻️ refactor(tests): clean up sitemap regression test
    - Remove try/except block used for initial failure demonstration
    - Simplify test assertion logic

--- pharmacies/tests/test_sitemaps.py
+++ pharmacies/tests/test_sitemaps.py
@@ -7,12 +7,6 @@ class SitemapTests(TestCase):
         """
         Test that the sitemap can be generated successfully.
         """
-        try:
-            response = self.client.get(reverse("django.contrib.sitemaps.views.sitemap"))
-            self.assertEqual(response.status_code, 200)
-            self.assertContains(response, "<loc>")
-        except Exception as e:
-            # We want to explicitly catch the NoReverseMatch if it bubbles up,
-            # but usually client.get catches it and returns 500 or raises it depending on test client setup.
-            # However, for the purpose of the 'Red' phase, simply running this and seeing it fail is enough.
-            raise e
+        response = self.client.get(reverse("django.contrib.sitemaps.views.sitemap"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<loc>")
